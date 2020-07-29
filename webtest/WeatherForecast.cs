d58515be8652ffc021aae1b3edc5112f2b127d2b
Carson <carson@CarsondeMacBook-Pro.local>
2020-07-29 22:35:59 +0800

    添加gitignore

--- webtest/WeatherForecast.cs
+++ webtest/WeatherForecast.cs
@@ -1,14 +1,17 @@
 using System;
 
-namespace webtest
-{
-    public class WeatherForecast
-    {
+namespace webtest {
+    public class WeatherForecast {
+        /// <summary>
+        /// 时间
+        /// </summary>
+        /// <value></value>
         public DateTime Date { get; set; }
+        public string Value { get; set; }
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 
         public string Summary { get; set; }
     }
